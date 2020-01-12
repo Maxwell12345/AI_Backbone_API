5825778e73ca181ab318a1776406b2c0c0aa5467
Maxwell <idlerm3@gmail.com>
2020-01-12 12:48:19 -0500

    Update BP_extern_files.hpp

--- Mat_mem/BP_extern_files.hpp
+++ Mat_mem/BP_extern_files.hpp
@@ -1,3 +1,3 @@
 #include "/Users/idler/Desktop/eigen/Eigen/Dense"
-#include "/Users/idler/Desktop/GitHub/AI_Backbone/Core/activation_data.hpp"
-#include "/Users/idler/Desktop/GitHub/AI_Backbone/Core/layer_matrix.hpp"
+#include "/Users/idler/Desktop/GitHub/AI_Backbone_API/Core/activation_data.hpp"
+#include "/Users/idler/Desktop/GitHub/AI_Backbone_API/Core/layer_matrix.hpp"
