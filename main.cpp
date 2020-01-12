54523431252e7a9266022ce7a14ab78abca349e8
Maxwell <idlerm3@gmail.com>
2020-01-12 12:48:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@ int main(int argc, const char **argv)
 {
     std::cout << argv[0] << std::endl;
     double **inp = (double **)malloc(sizeof(double **));
+    double **out = (double **)malloc(sizeof(double **));
 
     int j = 1;
     for(int i = 0; i < 3; ++i)
     {
         inp[i] = (double *)malloc(sizeof(double) * 3);
-        inp[i][0] = i + j; inp[i][1] = i + j + 1; inp[i][2] = i + j + 2;
+        inp[i][0] = 1; inp[i][1] = 2 + 1; inp[i][2] = 3;
         j += 1;
-    }
-    for(int i = 0 ; i < 3; ++i)
+    }j = 1;
+
+    for(int i = 0; i < 3; ++i)
     {
-        for(int j = 0; j < 3; ++j)
-        {
-            std::cout << inp[i][j] << " ";
-        }
-        std::cout << std::endl;
+        out[i] = (double *)malloc(sizeof(double) * 3);
+        out[i][0] = i + j; out[i][1] = i + j + 1; out[i][2] = i + j + 2;
+        j += 1;
     }
 
     unsigned inp_format[2] = {3, 3};
@@ -32,6 +32,7 @@ int main(int argc, const char **argv)
     model->add(3, "relu");
 
     model->initialize_network_input(inp);
+    model->initialize_network_output(out);
 
     model->initialize_global_variables();
     std::cout << "\n\n\n\n\n\n";
