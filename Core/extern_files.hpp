d69b2122b78ad3e077b5c5a243896fbece4bd182
Maxwell <idlerm3@gmail.com>
2020-01-12 12:48:08 -0500

    Update extern_files.hpp

--- Core/extern_files.hpp
+++ Core/extern_files.hpp
@@ -1,2 +1,2 @@
 #include "/Users/idler/Desktop/eigen/Eigen/Dense"
-#include "/Users/idler/Desktop/GitHub/AI_Backbone/Mat_mem/back_propagation.hpp"
+#include "/Users/idler/Desktop/GitHub/AI_Backbone_API/Mat_mem/back_propagation.hpp"
