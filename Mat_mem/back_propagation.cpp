eec2448a7efbaf864fa3813ffba3ef63cb0bd620
Maxwell <idlerm3@gmail.com>
2020-01-12 12:48:17 -0500

    Update back_propagation.cpp

--- Mat_mem/back_propagation.cpp
+++ Mat_mem/back_propagation.cpp
@@ -9,10 +9,12 @@ struct _update_variable_mat_
     // These need to be initiated first before any instance variable or method is used
     std::vector<unsigned> lSize_arr;
 
-    Eigen::Matrix<T, -1, 1> y_data;
+    std::vector<Eigen::Matrix<T, -1, 1> >y_data;
     std::string cost;
     std::vector<std::string> activation_func_arr;
+    uint32_t num_sets;
     T learning_rate;
+    uint32_t epoch_idx;
 
     inline void record_mat_data(Eigen::Matrix<T, -1, -1> *history);
     inline T get_error_val(Eigen::Matrix<T, -1, 1> outps, bool get_sumation, uint32_t index); // index is 0 if sumation is true
@@ -20,7 +22,8 @@ struct _update_variable_mat_
     inline T dirivative(T *, T, unsigned num_layers_to_backproagate, unsigned oNeuron_idx); 
     inline T dirivative(T *, uint32_t update_idx); //update_idx is the index of the deepest mat being updated
     // The pointer array goes in order from the output neuron to the weight that needs to be update
-    inline void format_y_data(T *);
-    inline void format_y_data(std::vector<T>);
+    inline void format_y_data(T **);
+    inline void format_y_data(std::vector<std::vector<T> >);
     inline void update_network_variables(Eigen::Matrix<T, -1, 1> neuronOutps);
+    inline void set_lArr(std::vector<unsigned>);
 };
